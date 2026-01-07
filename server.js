84c9518563baf6a5e22a63770ca44d2598a5f232
sachin94200333 <sachin94200333@gmail.com>
2026-01-07 13:08:25 +0530

    fixed

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ app.post('/api/about', async (req, res) => {
     res.json(about);
 });
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); });
-
+app.get('/*', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
